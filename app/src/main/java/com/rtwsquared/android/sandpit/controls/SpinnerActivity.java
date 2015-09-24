a3d9a899210e17fc1716a55d8be9ca68dccc8c0c
Rob Winston <robtwinston@gmail.com>
2015-09-24 15:43:12 +0100

    Initial SpinnerActivity setup

---
+++ app/src/main/java/com/rtwsquared/android/sandpit/controls/SpinnerActivity.java
@@ -0,0 +1,16 @@
+package com.rtwsquared.android.sandpit.controls;
+
+import android.os.Bundle;
+
+import com.rtwsquared.android.sandpit.R;
+import com.rtwsquared.android.util.activity.TraceBaseActivity;
+
+public class SpinnerActivity extends TraceBaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spinner);
+    }
+
+ }
